a24aeb01ca518e19e8535d5fbbd6ab9247bc0d85
Juhi22 <j.dani22@freemail.hu>
2025-12-06 10:28:57 +0100

    Update landing page UI, add mat card and mat buttons

--- FE/src/app/components/landing-page/landing-page.component.ts
+++ FE/src/app/components/landing-page/landing-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'landing-page',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
