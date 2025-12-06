8d39961545ce6a7f5e9f5262059eb483defd99d0
Juhi22 <j.dani22@freemail.hu>
2025-12-06 09:55:52 +0100

    Add landing-page component

--- FE/src/app/app.component.ts
+++ FE/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [LandingPageComponent],
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'demo-error-handling';
