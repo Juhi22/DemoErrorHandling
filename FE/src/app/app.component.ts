bb28299cfaf4f64b766d5b157bb93e90078502ba
Juhi22 <j.dani22@freemail.hu>
2025-12-06 14:15:43 +0100

    Remove unused import from app.component

--- FE/src/app/app.component.ts
+++ FE/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [LandingPageComponent, RouterOutlet],
+  imports: [RouterOutlet],
   styleUrl: './app.component.css',
 })
 export class AppComponent {
