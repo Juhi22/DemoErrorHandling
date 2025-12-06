f5f53af0b814c6d2fe0198524eb155c82f9c700b
Juhi22 <j.dani22@freemail.hu>
2025-12-06 14:12:53 +0100

    Move baseUrl from service to environment config file

--- FE/src/app/services/backend-api.service.ts
+++ FE/src/app/services/backend-api.service.ts
@@ -2,17 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DemoResponse } from '../interfaces/demo-response';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendApiService {
 
-  private baseUrl: string = "http://localhost:8080/api/demo"
-
   constructor(private httpClient: HttpClient) { }
 
   sendGetRequest(pathVariable: string): Observable<DemoResponse> {
-    return this.httpClient.get<DemoResponse>(`${this.baseUrl}/${pathVariable}`);
+    return this.httpClient.get<DemoResponse>(`${environment.apiBaseUrl}/${pathVariable}`);
   }
 }
