458a62b0579f6d02d07dcde79c0fae8894223f4d
Juhi22 <j.dani22@freemail.hu>
2025-12-06 12:55:04 +0100

    Configure CORS

--- BE/src/main/java/hu/errorhandling/demo/api/ErrorHandlingAPI.java
+++ BE/src/main/java/hu/errorhandling/demo/api/ErrorHandlingAPI.java
@@ -2,10 +2,12 @@ package hu.errorhandling.demo.api;
 
 import hu.errorhandling.demo.model.DemoResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping
+@CrossOrigin("*")
 public interface ErrorHandlingAPI {
 
     String API_PATH_PREFIX = "/api/demo";
